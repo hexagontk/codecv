fd85680cc7be93d7aff09e2c102c6be20f7415d7
jaguililla <juanjoaguililla@gmail.com>
2023-01-12 18:15:25 +0100

    Improve CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ apply(from = "$gradleScripts/kotlin.gradle")
 apply(from = "$gradleScripts/application.gradle")
 
 group = "com.hexagonkt.tools"
-version = "0.9.4"
+version = "0.9.5"
 description = "CVs for programmers"
 
 ext {
