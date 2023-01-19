96328f73117538a6a939c49b9950a0ef102e2d6c
jaguililla <juanjoaguililla@gmail.com>
2023-01-19 15:31:50 +0100

    Release native images

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ apply(from = "$gradleScripts/application.gradle")
 apply(from = "$gradleScripts/native.gradle")
 
 group = "com.hexagonkt.tools"
-version = "0.9.13"
+version = "0.9.14"
 description = "CVs for programmers"
 
 ext {
