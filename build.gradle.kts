ace134e06f170dea969011a1a6b9d455323db735
jaguililla <juanjoaguililla@gmail.com>
2023-01-19 09:33:10 +0100

    Fix build problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,8 +53,7 @@ dependencies {
 }
 
 tasks.named("classes") { dependsOn("addResources") }
-tasks.named("build") { dependsOn("tarJpackage", "metadataCopy") }
-tasks.named("nativeCompile") { dependsOn("metadataCopy") }
+tasks.named("build") { dependsOn("tarJpackage") }
 
 tasks.create<Copy>("addResources") {
     from(projectDir)
