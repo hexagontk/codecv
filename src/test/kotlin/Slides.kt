6f4a92ef0ead07b02210bd75957d53a5bc019c4c
jaguililla <juanjoaguililla@gmail.com>
2023-10-20 17:59:06 +0200

    Fix CI

--- src/test/kotlin/Slides.kt
+++ src/test/kotlin/Slides.kt
@@ -3,13 +3,10 @@ package co.codecv
 import com.hexagonkt.http.server.HttpServerSettings
 import com.hexagonkt.http.server.callbacks.FileCallback
 import com.hexagonkt.http.server.netty.serve
-import java.awt.Desktop
 import java.io.File
-import java.net.URI
 
 fun main() {
-    val server = serve(HttpServerSettings(bindPort = 9999)) {
+    serve(HttpServerSettings(bindPort = 9999)) {
         get("/*", FileCallback(File("src/test/resources")))
     }
-    Desktop.getDesktop().browse(URI("http://localhost:${server.runtimePort}/revealjs.html"))
 }
